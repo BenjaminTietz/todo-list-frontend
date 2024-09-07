b18691501f559ee52c2597aadfe945fe5b696a12
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-09-07 14:22:58 +0200

    feat: fetching and displaying all Todos in AllTodosComponent
    - Implemented `ngOnInit()` in `AllTodosComponent` to fetch all todos from the API on component initialization.
    - Used `loadTodos()` method to make an HTTP GET request to the `/todos/` endpoint, retrieving the data from the backend.
    - Displayed the fetched todos dynamically in the template using a `*ngFor` loop.
    - Each todo is rendered with a checkbox reflecting its `checked` status and its title.
    - The `trackBy` logic improves performance by efficiently tracking rendered items.

--- src/app/components/all-todos/all-todos.component.ts
+++ src/app/components/all-todos/all-todos.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../../environmets/enviroment';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-all-todos',
   standalone: true,
   imports: [],
   templateUrl: './all-todos.component.html',
-  styleUrl: './all-todos.component.scss'
+  styleUrl: './all-todos.component.scss',
 })
-export class AllTodosComponent {
+export class AllTodosComponent implements OnInit {
+  todos: any = [];
+  constructor(private http: HttpClient) {}
 
+  async ngOnInit() {
+    this.todos = await this.loadTodos();
+    console.log(this.todos);
+  }
+
+  loadTodos() {
+    const url = environment.baseUrl + '/todos/';
+    return lastValueFrom(this.http.get(url));
+  }
 }
